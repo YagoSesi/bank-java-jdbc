89d431fa2846de0c7ebffea2414305c31dfd295d
Yago <yago1452700@sesigoias.com.br>
2024-10-14 10:13:55 -0300

    Subindo os arquivos

--- src/main/java/br/sesi/bank/bank_java_jdbc/domain/cliente/DadosCadastradosCliente.java
+++ src/main/java/br/sesi/bank/bank_java_jdbc/domain/cliente/DadosCadastradosCliente.java
@@ -1,4 +1,13 @@
 package br.sesi.bank.bank_java_jdbc.domain.cliente;
 
 public class DadosCadastradosCliente {
+    public String nome;
+    public String cpf;
+    public String email;
+
+    public DadosCadastroCliente(nome String, cpf String, email String)
+
+
+
 }
+
--- src/main/java/br/sesi/bank/bank_java_jdbc/domain/conta/DadosAberturaConta.java
+++ src/main/java/br/sesi/bank/bank_java_jdbc/domain/conta/DadosAberturaConta.java
@@ -1,4 +1,15 @@
 package br.sesi.bank.bank_java_jdbc.domain.conta;
 
+import br.sesi.bank.bank_java_jdbc.domain.cliente.DadosCadastradosCliente;
+
 public class DadosAberturaConta {
+    public Integer numero;
+    public DadosCadastradosCliente dadosCliente;
+
+
+    public DadosAberturaConta(Integer numero, DadosCadastradosCliente dadosCliente){
+        this.numero = numero;
+        this.dadosCliente = dadosCliente;
+    }
+
 }
